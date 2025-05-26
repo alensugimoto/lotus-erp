a9ca5ce23ce081eba8197d29b5058899819b0a55
Alen Sugimoto <alensugimoto@gmail.com>
2025-05-26 10:18:52 -0700

    :technologist: Use watchexec instead of olive

---
+++ wc.sh
@@ -0,0 +1,1 @@
+cd client && watchexec --wrap-process session --exts gleam -- "gleam run -m lustre/dev build"
---
+++ ws.sh
@@ -0,0 +1,1 @@
+watchexec --restart --wrap-process=session --exts=gleam,toml --ignore=*/manifest.toml -- "cd server && gleam run"
