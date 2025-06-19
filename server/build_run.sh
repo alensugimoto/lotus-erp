c1250210cca78ec9fba366cf22b7d31d0d4da961
Alen Sugimoto <alensugimoto@gmail.com>
2025-06-19 11:42:35 -0700

    :technologist: Add back dev_client to watch script

--- server/build_run.sh
+++ server/build_run.sh
@@ -1,4 +1,6 @@
-cd ../client &&
+cd ../dev_client &&
+  gleam run -m lustre/dev build &&
+  cd ../client &&
   gleam run -m lustre/dev build &&
   cd ../server &&
   gleam run
--- watch_build_run.sh
+++ watch_build_run.sh
@@ -1,9 +1,14 @@
 # NOTE: lustre/dev needs to be run even on server-only changes
 # because tailwind reads server source files.
 # NOTE: change to server directory to ensure server env is set up before running the server.
+# TODO: when only dev_client changes,
+# no need to run `lustre/dev build` on client.
+# Same for other more minor cases.
+# Wait for issue to resolve: https://github.com/watchexec/watchexec/issues/33.
 cd server &&
   watchexec \
     --wrap-process=session \
+    --watch=../dev_client/src \
     --watch=../client/src \
     --watch=src \
     --exts=gleam,mjs,erl \
